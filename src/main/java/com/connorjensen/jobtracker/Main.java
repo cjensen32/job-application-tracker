6715920b61336d6f0c7b93da7210272c0c5cefac
Connor Jensen <313645175+cjensen32@users.noreply.github.com>
2026-08-16 11:52:56 -0500

    CAPSTONE(ch 1) - stopping place before asking for guideance

--- src/main/java/com/connorjensen/jobtracker/Main.java
+++ src/main/java/com/connorjensen/jobtracker/Main.java
@@ -9,54 +9,107 @@ import com.connorjensen.jobtracker.repository.InMemoryApplicationRepository;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Optional;
+import java.util.Scanner;
 
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        
+        // Activates the repository and service to store/manage apps
         ApplicationRepository repository = new InMemoryApplicationRepository();
         ApplicationService service = new ApplicationService(repository);
-        List<Application> applications = new ArrayList<>();
-        applications.add(
-                new Application(
-                    "Acme Corp", 
-                    "Backend Engineer", 
-                    LocalDate.of(2026, 8, 1)
-                    )
-                );
-        applications.add(
-                new Application(
-                    "Globex", 
-                    "Platform Engineer", 
-                    LocalDate.of(2026, 8, 5)
-                    )
-                );
-        applications.add(
-                new Application(
-                    "Initech",
-                    "Java Developer", 
-                    LocalDate.of(2026, 8, 9)
-                    )
-                );
-        applications.get(1).setStatus(Status.INTERVIEWING);
 
-        for (Application app : applications) {
-            System.out.println(app.summary() + " [" + app.getStatus() + "]");
+        // Print first time statements
+        printHeader();
+        printOptions();
+
+        int selection = 0;
+
+        // Switch to continue with process
+        while (selection != 5) {
+            selection = takeInput();
+
+            Application result = switch (selection) {
+                case 0 -> ApplicationEntry(service);
+                default -> ApplicationEntry(service);
+            };
+            
         }
+    }
 
-        List<Application> interviewing = applications.stream()
-            .filter(app -> app.getStatus() == Status.INTERVIEWING)
-            .toList();
+    public static void printHeader() {
+        System.out.print("""
 
-        Optional<Application> found = applications.stream()
-            .filter(app -> app.getCompany().equals("Globex"))
-            .findFirst();
+                ==============================================
+                Welcome to Job Application Tracker!!
+                What would you like to do?
+                ==============================================
+                """); 
+    }
 
-        Optional<Application> missing = applications.stream()
-            .filter(app -> app.getCompany().equals("Nope Inc"))
-            .findFirst();
-        System.out.println("missing: " + missing.map(Application::getCompany).orElse("not found"));
+    public static void printOptions() {
+        List<String> options = new ArrayList<>(
+                List.of(
+                    "Create new application",  
+                    "List all applications", 
+                    "List applications with status",  
+                    "Update status on application",
+                    "Delete application by id",
+                    "Quit Application"
+                    )
+                );
 
-        System.out.println("Found Globex entries: " + found.map(Application::getCompany).orElse("not found"));
-        System.out.println("interviewing: " + interviewing.size());
+        for (int i = 0; i < options.size(); i++) { 
+            System.out.println(" (" + i + "): " +  options.get(i));
+        }
+        System.out.print("> ");
+    }
+
+    public static void printOptionsError() {
+        System.out.print("""
+                ==============================================
+                ERROR IN ENTRY: Please try to enter again!!
+                ==============================================
+                """); 
+            printOptions();
+    }
+
+    public static int takeInput() {
+        boolean success = false;  
+        int selection = 6; // Number out of range of options; keeps selection initialized 
+        while (!success) {
+            try {
+                selection = scanner.nextInt();
+                if (selection < 5 && selection >= 0) {
+                    success = true;
+                } else {
+                    printOptionsError();
+                }
+            } catch(InputMismatchException e) {
+                printOptionsError();
+                scanner.nextLine();
+            }
+        }
+        return selection;
+    }
+
+    public static Application ApplicationEntry(ApplicationService service) {
+        scanner.nextLine(); // Clear the buffer
+
+        System.out.print("Enter company name:\n> ");
+        String company = scanner.nextLine();
+
+        System.out.print("Enter job name:\n> ");
+        String role = scanner.nextLine();
+
+        System.out.print("Enter applied date (YYYY-MM-DD):\n> ");
+        String stringDate = scanner.nextLine();
+        LocalDate appliedDate = LocalDate.parse(stringDate);
+        
+        Application result = service.create(company, role, appliedDate);
+        System.out.print(result);
+        return result;
     }
 }
